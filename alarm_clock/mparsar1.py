7d7783e6755ac0027d8296bdd973dd099dec7803
Joseph Flanagan <gamblinflanagan97@gmail.com>
2021-01-06 10:44:30 -0500

    Update mparsar1.py
    The information that is parsed changes location when Accuweather updates their website this will work with the latest version as well as future versions

--- alarm_clock/mparsar1.py
+++ alarm_clock/mparsar1.py
@@ -2,8 +2,11 @@ import sys
 import xml.dom.minidom
 from xml.dom.minidom import parse
 
+#Mostly, Cloudy, Pleasent, Beautiful, Patchy
+weatherlst = ['°' , 'RealFeel®', '%', 'M', 'S', 'C', 'P', 'B', 'T']
 lst = []
 lst_final = []
+#testlst = []
 document = xml.dom.minidom.parse(sys.argv[1])
 elements = document.getElementsByTagName('body')
 counter = 0
@@ -14,54 +17,38 @@ for div1 in elements[0].getElementsByTagName('div'):
             for div4 in div3.getElementsByTagName('div'):
                 for div5 in div4.getElementsByTagName('div'):
                     for div6 in div5.getElementsByTagName('div'):
-                        for div7 in div6.getElementsByTagName('div'):
-                            for div8 in div7.getElementsByTagName('div'):
-                                for div9 in div8.getElementsByTagName('div'):
-                                    for div11 in div9.getElementsByTagName('div'):
-                                        for p1 in div11.getElementsByTagName('p'):
-                                            for node in p1.childNodes:
-                                                if node.nodeType == node.TEXT_NODE:
-                                                    lst.append(node.nodeValue)#data.append(node.nodeValue)
-                                                    #continue
-                                                    
-                                for node in div8.childNodes:
-                                    if node.nodeType == node.TEXT_NODE:
-                                        lst.append(node.nodeValue)#data.append(node.nodeValue)
-                                        #continue
-                                        
-                        for div12 in div6.getElementsByTagName('div'):
-                            for div13 in div12.getElementsByTagName('div'):
-                                for div14 in div13.getElementsByTagName('div'):
-                                    for p2 in div14.getElementsByTagName('p'):
-                                         for node in p2.childNodes:
-                                             if node.nodeType == node.TEXT_NODE:
-                                                 lst.append(node.nodeValue)#data.append(node.nodeValue)
-                                                 #continue
-try:                                   
-    lst_final.append(lst[4340])
-    lst_final.append(lst[4342]) #" degrees real feel in the shade " + lst_final[2][-8:-6] +
-    lst_final.append(lst[4343])
-    lst_final.append(lst[4357])
-    lst_final.append(lst[4421])
-
-    '''
-    x = 0
-    for i in lst:
-        if 'Humidity: 89%' in i:
-            if 'to partly cloudy' not in i:
-                x = lst.index(i)
-    print(str(x) + '\n')
-    '''
+                        for node in div6.childNodes:
+                            if node.nodeType == node.TEXT_NODE:
+                                x = str(node.nodeValue)
+                                for i in weatherlst:
+                                    if i in x:
+                                        if x not in lst:
+                                            lst.append(x)#data.append(node.nodeValue)    
+                                            #continue
+                           
+#y = lst                  
+#print(y)
+#print('\n')
+try:
+                               
+    lst_final.append(lst[1])
+    lst_final.append(lst[2])
+    lst_final.append(lst[4])
+    lst_final.append(lst[8])
+    
 
     inf = open("location.txt", "r")
     location = inf.read()
     inf.close()
-
-    st = "currently in " + location + " it is " + lst_final[0][-3:-1] + " degrees real feel " + lst_final[1][-7:-5] + " degrees and " + lst_final[3] + " with " + lst_final[4][-6:-4] + " percent humidity, "    
+    
+    
+    
+    st = "currently in " + location + " it is " + lst_final[0][-3:-1] + " degrees real feel " + lst_final[1][-3:-1] + " degrees and " + lst_final[2] + " with " + lst_final[3][0:2] + " percent humidity, "    
 except:
     st = "Sir, you have my sinseerest apologies, as i am currently unable to obtain the weather"
 
 
+#print(str(lst_final)+'\n')
 
 outf = open("morning_main2.txt", "w+")
 outf.write(str(st))
